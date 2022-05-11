62f068b4bfb9a25a2b5a7f029e89ca844d8fbba3
sergio2000 <42353861+sergio2000@users.noreply.github.com>
2022-05-11 00:26:03 -0700

    Initial Commit

---
+++ PlanoramaApp/PlanoramaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanoramaAppApp.swift
+//  PlanoramaApp
+//
+//  Created by Sergio Acosta on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PlanoramaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
